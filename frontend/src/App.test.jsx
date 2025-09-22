97b5a8e0b7b11d525a692fa2567fa5b9a7d25b16
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-09-22 14:38:21 +0200

    Fix EsLint

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react"
-import App from "./App"
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
 
 test("renders Bookstore heading", () => {
-    render(<App />)
-    expect(screen.getByText(/bookstore/i)).toBeInTheDocument()
-})
+  render(<App />);
+  expect(screen.getByText(/bookstore/i)).toBeInTheDocument();
+});
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { StyledEngineProvider } from "@mui/material/styles";
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App.jsx";
+import "./index.css";
 import theme from "./theme.js";
 
 createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <App />
-            </ThemeProvider>
-        </StyledEngineProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </React.StrictMode>,
 );
