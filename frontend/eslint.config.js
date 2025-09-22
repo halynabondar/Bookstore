f4f2917f39dfc0cc833e70020c337845cbe754c1
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-09-22 11:25:37 +0200

    chore(eslint): extend config with react and jsx-a11y plugins, add ignores for node_modules and coverage

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,17 +1,21 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', 'coverage']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      jsxA11y.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -20,6 +24,11 @@ export default defineConfig([
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
     rules: {
