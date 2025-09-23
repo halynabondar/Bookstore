a3ae5f847e3074e9318ee7819ebea127d74f5737
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-09-23 09:40:35 +0200

    Fix ESlint

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react'
 
-import App from "./App";
+import App from './App'
 
-test("renders Bookstore heading", () => {
-  render(<App />);
-  expect(screen.getByText(/bookstore/i)).toBeInTheDocument();
-});
+test('renders Bookstore heading', () => {
+  render(<App />)
+  expect(screen.getByText(/bookstore/i)).toBeInTheDocument()
+})
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import { StyledEngineProvider } from "@mui/material/styles";
-import React from "react";
-import { createRoot } from "react-dom/client";
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { StyledEngineProvider } from '@mui/material/styles'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-import App from "./App.jsx";
-import "./index.css";
-import theme from "./theme.js";
+import App from './App.jsx'
+import './index.css'
+import theme from './theme.js'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
@@ -15,5 +15,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </ThemeProvider>
     </StyledEngineProvider>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
