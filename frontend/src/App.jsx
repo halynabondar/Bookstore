077e9a57c88fdad24eb93f25bea05c9ce930fbd5
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-09-24 13:16:45 +0200

    feat: add Navbar component and correct import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
+import Footer from './components/Footer.jsx'
+import MainImage from './components/HeroImage.jsx'
 import Navbar from './components/Navbar.jsx'
 
 function App() {
+  const navList = ['Books', 'Shop', 'About', 'Blog', 'Contact us']
+  const footerList = [
+    'Style Guide',
+    'Licesing',
+    'Changelog',
+    'Error 404',
+    'Password',
+    'Privacy Policy',
+    'Return Policy',
+    'Coming Soon',
+  ]
+
   return (
     <>
-      <Navbar />
+      <Navbar navList={navList} />
+      <MainImage />
+      <main className="mx-auto max-w-7xl"></main>
+      <Footer navList={navList} footerList={footerList} />
     </>
   )
 }
