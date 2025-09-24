b4c1477c9b4f82f6f5501221607383c706748590
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-09-24 13:51:58 +0200

    Minor fix

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer({ navList, footerList }) {
   return (
     <footer className="bg-dark-100 text-primary-dark">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:p-10">
-        <div className="flex justify-around gap-14 md:flex-row flex-col">
-          <div className="w-full md:w-1/2 space-y-8">
+        <div className="flex flex-col justify-around gap-14 md:flex-row">
+          <div className="w-full space-y-8 md:w-1/2">
             <div>
               <a
                 href="/"
@@ -18,14 +18,17 @@ export default function Footer({ navList, footerList }) {
             <h2 className="font-sans text-4xl text-primary-dark">
               Start Your Empowering Reading Adventure
             </h2>
-            <button type="button" className="flex gap-2 rounded-2xl bg-primary-dark px-3 py-2 text-textc-onDark transition duration-300 hover:bg-primary-light">
+            <button
+              type="button"
+              className="flex gap-2 rounded-2xl bg-primary-dark px-3 py-2 text-textc-onDark transition duration-300 hover:bg-primary-light"
+            >
               Contact us
               <ArrowForwardIcon />
             </button>
           </div>
-          <div className="flex gap-14 flex-row md:justify-between">
-            <div className='w-full md:w-1/2 space-y-8'>
-              <h3 className="font-mono text-lg font-bold leading-none text-primary-dark">
+          <div className="flex flex-row gap-14 md:justify-between">
+            <div className="w-full space-y-8 md:w-1/2">
+              <h3 className="whitespace-nowrap font-mono text-lg font-bold leading-none text-primary-dark">
                 Quick Links
               </h3>
               <ul className="mt-4 space-y-2">
@@ -39,7 +42,7 @@ export default function Footer({ navList, footerList }) {
                 ))}
               </ul>
             </div>
-            <div className='w-full md:w-1/2'>
+            <div className="w-full md:w-1/2">
               <ul className="space-y-2">
                 {footerList.map(item => (
                   <li
