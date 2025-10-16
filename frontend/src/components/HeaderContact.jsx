a62fbcb70a8babbbe31f3724f012c0becb5d4026
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-10-16 10:56:15 +0200

    Add HeaderContact component with responsive layout and intro text

---
+++ frontend/src/components/HeaderContact.jsx
@@ -0,0 +1,10 @@
+export default function HeaderContactUs() {
+    return (
+        <div className='w-full bg-secondary-light/60'>
+            <div className='flex flex-col md:flex-row max-w-7xl mx-auto h-auto md:h-56 px-4 py-8 sm:px-6 lg:px-10 gap-4 md:gap-16 items-start md:items-center justify-center'>
+                <h1 className="w-full md:w-1/2 text-4xl md:text-5xl text-nowrap">Get in Touch</h1>
+                <p className='w-full md:w-1/2 flex text-lg text-left md:text-right'>Connect with us and share your thoughts, questions, or feedback. We're here to assist you!</p>
+            </div>
+        </div>
+    )
+}
