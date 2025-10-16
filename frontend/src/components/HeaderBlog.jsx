b36d156b51819d611d26d6b52a85e4d95c10d870
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-10-16 10:56:40 +0200

    Add HeaderBlog component with responsive layout and intro text

---
+++ frontend/src/components/HeaderBlog.jsx
@@ -0,0 +1,10 @@
+export default function HeaderBlog() {
+    return (
+        <div className='w-full bg-secondary-light/60'>
+            <div className='flex flex-col md:flex-row max-w-7xl mx-auto h-auto md:h-56 px-4 py-8 sm:px-6 lg:px-10 gap-4 md:gap-16 items-start md:items-center justify-center'>
+                <h1 className="w-full md:w-1/2 text-4xl md:text-5xl text-nowrap">Our Blogs</h1>
+                <p className='w-full md:w-1/2 flex text-lg text-left md:text-right'>In a world that can sometimes feel overwhelming and challenging, positive thinking offers a ray of hope and a path to a brighter future.</p>
+            </div>
+        </div>
+    )
+}
