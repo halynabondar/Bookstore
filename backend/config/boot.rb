93daa624d8d310a99fe5bb559eef210b669f5128
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-10-27 14:07:40 +0100

    Disable Bootsnap in CI and test environments to fix GitHub Actions error

--- backend/config/boot.rb
+++ backend/config/boot.rb
@@ -3,7 +3,7 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 
-# Disable Bootsnap in test environment (it causes FrozenError sometimes)
-unless ENV["RAILS_ENV"] == "test"
+# Disable Bootsnap when running in CI or test mode
+if ENV["CI"].nil? && ENV["RAILS_ENV"] != "test"
   require "bootsnap/setup"
 end
