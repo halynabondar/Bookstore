c05b31493e00a2fe78c532c6ea015e8662f1158d
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-10-27 14:25:26 +0100

    Disable Bootsnap completely in CI and test environments

--- backend/config/boot.rb
+++ backend/config/boot.rb
@@ -3,7 +3,7 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 
-# Disable Bootsnap when running in CI or test mode
-if ENV["CI"].nil? && ENV["RAILS_ENV"] != "test"
+# Disable Bootsnap when running in test or CI
+if ENV["RAILS_ENV"] != "test" && !ENV["CI"]
   require "bootsnap/setup"
 end
