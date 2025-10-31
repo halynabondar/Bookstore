eeb1277567f7fc6523fe90143f0a5467335347bc
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-10-31 14:42:01 +0100

    Create a Sign in page

---
+++ frontend/src/pages/SignInPage.jsx
@@ -0,0 +1,9 @@
+import SignInForm from "../components/SignInForm.jsx";
+
+export default function SignInPage() {
+    return (
+        <section className=''>
+            <SignInForm/>
+        </section>
+    )
+}
