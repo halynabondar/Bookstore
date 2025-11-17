d2916f4bd1d796c9f8e63bc4d9e15e24cc33bc5d
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-11-17 16:52:26 +0100

    chore: lint and format code

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,29 +1,29 @@
-import {Navigate} from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
-import {useUser} from '../hooks'
+import { useUser } from '../hooks'
 
 export default function ProfilePage() {
-    const {user} = useUser()
+  const { user } = useUser()
 
-    if (!user) return <Navigate to="/signin" replace/>
+  if (!user) return <Navigate to="/signin" replace />
 
-    return (
-        <section className="mx-auto max-w-xl px-4 py-8">
-            <h1 className="mb-4 text-3xl font-semibold text-dark-700">My Profile</h1>
-            <div className="rounded-xl bg-dark-100 p-6 shadow">
-                <p className="mb-2">
-                    <span className="font-medium">ID:</span> {user.id}
-                </p>
-                <p className="mb-2">
-                    <span className="font-medium">Email:</span> {user.email}
-                </p>
-                <p className="mb-2">
-                    <span className="font-medium">First name:</span> {user.first_name}
-                </p>
-                <p className="mb-2">
-                    <span className="font-medium">Last name:</span> {user.last_name}
-                </p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="mx-auto max-w-xl px-4 py-8">
+      <h1 className="mb-4 text-3xl font-semibold text-dark-700">My Profile</h1>
+      <div className="rounded-xl bg-dark-100 p-6 shadow">
+        <p className="mb-2">
+          <span className="font-medium">ID:</span> {user.id}
+        </p>
+        <p className="mb-2">
+          <span className="font-medium">Email:</span> {user.email}
+        </p>
+        <p className="mb-2">
+          <span className="font-medium">First name:</span> {user.first_name}
+        </p>
+        <p className="mb-2">
+          <span className="font-medium">Last name:</span> {user.last_name}
+        </p>
+      </div>
+    </section>
+  )
 }
