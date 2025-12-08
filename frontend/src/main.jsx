941ba3df88d637ca521015b3b189c8df5ca65263
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-12-08 15:20:25 +0100

    chore: lint and format code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,24 @@
-import {CssBaseline} from '@mui/material'
-import {StyledEngineProvider} from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import { StyledEngineProvider } from '@mui/material/styles'
 import React from 'react'
-import {createRoot} from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 import './index.css'
-import {BooksProvider, UserProvider} from './context'
+import { BooksProvider, UserProvider } from './context'
 
 createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <StyledEngineProvider injectFirst>
-            <CssBaseline/>
-            <BrowserRouter>
-                <UserProvider>
-                    <BooksProvider>
-                        <App/>
-                    </BooksProvider>
-                </UserProvider>
-            </BrowserRouter>
-        </StyledEngineProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      <BrowserRouter>
+        <UserProvider>
+          <BooksProvider>
+            <App />
+          </BooksProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </StyledEngineProvider>
+  </React.StrictMode>
 )
