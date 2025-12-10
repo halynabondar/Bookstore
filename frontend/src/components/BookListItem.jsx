0793eca17ca0fcd6f83583bd9b8f6d63bbfb764c
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-12-10 11:19:08 +0100

    Add image

---
+++ frontend/src/components/BookListItem.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types'
+
+export default function BookListItem({
+  title,
+  genre,
+  author,
+  price,
+  averageReviewScore,
+  numberOfReview,
+}) {
+  return (
+    <div className="flex h-80 w-60 flex-col rounded-xl ">
+      <img
+        src="../../public/books/bookCover.jpg"
+        alt={title}
+        className="mb-2 h-full w-full rounded-md object-cover"
+      />
+      <div className="flex gap-2 text-sm">
+        {averageReviewScore != null && <span>⭐ {averageReviewScore}</span>}
+        {numberOfReview != null && <span>({numberOfReview} reviews)</span>}
+      </div>
+      <p className="mt-2 text-xs uppercase text-gray-600">{genre}</p>
+      <h3 className="text-lg font-semibold">{title}</h3>
+      <p className="text-sm text-gray-700">{author}</p>
+      <p className="mt-auto text-sm font-semibold">{price} kr</p>
+    </div>
+  )
+}
+
+BookListItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  averageReviewScore: PropTypes.number.isRequired,
+  numberOfReview: PropTypes.number.isRequired,
+}
