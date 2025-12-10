a841232c8560ca7c5c9e6d71eb5f83c3ea6ad80d
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-12-10 11:30:30 +0100

    Add Filter component

---
+++ frontend/src/components/Filter.jsx
@@ -0,0 +1,15 @@
+export default function Filter() {
+  return (
+    <section>
+      <div>
+        <div>Categories</div>
+        <div>Book format</div>
+        <div>Publisher</div>
+        <div>Years</div>
+        <div>Price range</div>
+      </div>
+      <button>Search</button>
+      <button>Reset</button>
+    </section>
+  )
+}
