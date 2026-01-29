fcf3b50c7ec8db1239a777fb22fd5b7c7ef243a5
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-01-29 09:44:17 +0100

    Update some styles

--- frontend/src/components/BooksList.jsx
+++ frontend/src/components/BooksList.jsx
@@ -1,32 +1,35 @@
-import { useBooks } from '../hooks/index.js'
+import {useBooks} from '../hooks/index.js'
 
 import BookListItem from './BookListItem.jsx'
 
 export default function BooksList() {
-  const { books } = useBooks()
+    const {books} = useBooks()
 
-  if (!books || books.length === 0) {
+    if (!books || books.length === 0) {
+        return (
+            <section className="px-48 py-16">
+                <p>No books available yet.</p>
+            </section>
+        )
+    }
+
     return (
-      <section className="px-48 py-16">
-        <p>No books available yet.</p>
-      </section>
+        <section>
+            <h2 className='text-primary-dark text-4xl mb-8'>Books</h2>
+            <div className="flex flex-wrap justify-between gap-8">
+                {books.map(book => (
+                    <BookListItem
+                        key={book.id}
+                        title={book.title}
+                        genre={book.genre}
+                        author={book.author}
+                        icon={book.icon}
+                        price={book.price}
+                        averageReviewScore={book.average_review_score}
+                        numberOfReview={book.number_of_review}
+                    />
+                ))}
+            </div>
+        </section>
     )
-  }
-
-  return (
-    <section className="flex flex-wrap justify-between gap-8">
-      {books.map(book => (
-        <BookListItem
-          key={book.id}
-          title={book.title}
-          genre={book.genre}
-          author={book.author}
-          icon={book.icon}
-          price={book.price}
-          averageReviewScore={book.average_review_score}
-          numberOfReview={book.number_of_review}
-        />
-      ))}
-    </section>
-  )
 }
