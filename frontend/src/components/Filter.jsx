b61d1fca7b05a91e78f006b89db9191de68a20f2
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-01-29 09:44:19 +0100

    Update some styles

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -1,15 +1,16 @@
 export default function Filter() {
-  return (
-    <section>
-      <div>
-        <div>Categories</div>
-        <div>Book format</div>
-        <div>Publisher</div>
-        <div>Years</div>
-        <div>Price range</div>
-      </div>
-      <button>Search</button>
-      <button>Reset</button>
-    </section>
-  )
+    return (
+        <section>
+            <h2 className='text-primary-dark text-4xl mb-8'>Filter</h2>
+            <div>
+                <div>Categories</div>
+                <div>Book format</div>
+                <div>Publisher</div>
+                <div>Years</div>
+                <div>Price range</div>
+            </div>
+            <button>Search</button>
+            <button>Reset</button>
+        </section>
+    )
 }
