bd88e4451664601b1ae48731d28765d17a60bfe1
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-03-06 14:46:43 +0100

    chore: lint and format code

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types'
 
-export default function Button({children, type = 'button', onClick, disabled = false, className = ''}) {
+export default function Button({
+                                   children,
+                                   type = 'button',
+                                   onClick,
+                                   disabled = false,
+                                   className = '',
+                               }) {
     return (
         <button
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={`rounded-2xl bg-primary-light px-8 py-2 text-dark-100 ${className}`}>
+            className={`rounded-2xl bg-primary-light px-8 py-2 text-dark-100 ${className}`}
+        >
             {children}
         </button>
     )
@@ -17,4 +24,5 @@ Button.propTypes = {
     type: PropTypes.string,
     onClick: PropTypes.func,
     disabled: PropTypes.bool,
+    className: PropTypes.string,
 }
