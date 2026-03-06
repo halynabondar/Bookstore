8adec4030e637b1b051c25280a673a5ff3d7e57b
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-03-06 14:41:47 +0100

    Create reusable Button component

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+
+export default function Button({children, type = 'button', onClick, disabled = false, className = ''}) {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+            className={`rounded-2xl bg-primary-light px-8 py-2 text-dark-100 ${className}`}>
+            {children}
+        </button>
+    )
+}
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    type: PropTypes.string,
+    onClick: PropTypes.func,
+    disabled: PropTypes.bool,
+}
