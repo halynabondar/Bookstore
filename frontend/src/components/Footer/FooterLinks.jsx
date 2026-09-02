28208645ecc15458beb573cb13fd42caddd45a97
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-09-02 15:31:58 +0200

    fix: add footer links prop validation

--- frontend/src/components/Footer/FooterLinks.jsx
+++ frontend/src/components/Footer/FooterLinks.jsx
@@ -1,24 +1,35 @@
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 export default function FooterLinks({ title, links }) {
-  return (
-    <div className="w-full space-y-8 md:w-1/2">
-      <h3 className="whitespace-nowrap font-mono text-lg font-bold leading-none text-primary-dark">
-        {title}
-      </h3>
+    return (
+        <div className="w-full space-y-8 md:w-1/2">
+            <h3 className="whitespace-nowrap font-mono text-lg font-bold leading-none text-primary-dark">
+                {title}
+            </h3>
 
-      <ul className="space-y-2">
-        {links.map(link => (
-          <li key={link.name}>
-            <Link
-              to={link.path}
-              className="text-primary-dark transition-colors duration-300 hover:text-secondary-dark"
-            >
-              {link.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+            <ul className="space-y-2">
+                {links.map(link => (
+                    <li key={link.name}>
+                        <Link
+                            to={link.path}
+                            className="text-primary-dark transition-colors duration-300 hover:text-secondary-dark"
+                        >
+                            {link.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+FooterLinks.propTypes = {
+    title: PropTypes.string.isRequired,
+    links: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            path: PropTypes.string.isRequired,
+        })
+    ).isRequired,
 }
