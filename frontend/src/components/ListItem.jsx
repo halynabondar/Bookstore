b7115501b3824016cf2618a3b92e54d547e59c36
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-09-02 15:24:30 +0200

    Delete redundant component

--- frontend/src/components/ListItem.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export default function ListItem({ item }) {
-  return <li className="cursor-pointer">{item}</li>
-}
-
-ListItem.propTypes = {
-  item: PropTypes.string.isRequired,
-}
